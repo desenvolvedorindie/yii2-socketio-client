cbc5a69f8fc0a51b333d7bdd9e098e88dc7ac0ea
José Alberto Araujo Castro <albertocastrobj@gmail.com>
2018-07-10 15:37:10 -0300

    Add socket

---
+++ src/SocketIOClientAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace webrtc\web;
+
+use yii\web\AssetBundle;
+
+class SocketIOClientAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/socket.io-client/dist';
+
+    public function init(){
+        $this->js = YII_DEBUG ? [
+            'socket.io.dev.js',
+        ] : [
+            'socket.io.js',
+        ];
+    }
+}
